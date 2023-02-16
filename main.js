439d7ba9d644ed42c93295b0e79513a00dec144c
wili1991 <wikki9106@gmail.com>
2023-02-15 21:42:21 -0500

    porcentaje notas

--- main.js
+++ main.js
@@ -11,12 +11,12 @@
 // Una farmacia aplica al precio de los remedios el 10% de descuento, hacer un programa que
 // ingresando el costo de los medicamentos calcules el descuento y el precio final.
 
-let precio=parseInt( prompt("Ingrese el costo de los medicamentos"));
-let descuento;
-let preciofinal;
-descuento=precio*0.1
-preciofinal=precio-descuento;
-console.log(`Los medicamentos costaron ${precio} el descuento del 10% es: ${descuento} y el precio final es: ${preciofinal}`);
+// let precio=parseInt( prompt("Ingrese el costo de los medicamentos"));
+// let descuento;
+// let preciofinal;
+// descuento=precio*0.1
+// preciofinal=precio-descuento;
+// console.log(`Los medicamentos costaron ${precio} el descuento del 10% es: ${descuento} y el precio final es: ${preciofinal}`);
 
 
 
@@ -24,4 +24,18 @@ console.log(`Los medicamentos costaron ${precio} el descuento del 10% es: ${desc
 // • 55% del promedio final de sus calificaciones de los tres(3) parciales.
 // • 30% de la calificación examen final y
 // • 15% de la calificación trabajo final.
+
+let nota1=parseFloat(prompt("ingrese la nota 1 del parcial"));
+let nota2=parseFloat(prompt("ingrese la nota 2 del parcial"));
+let nota3=parseFloat(prompt("ingrese la nota 3 del parcial"));
+let examenf=parseFloat(prompt("Ingrese la calificación del examen final"));
+let trabajof=parseFloat(prompt("Ingrese la calificación del trabajo final"));
+let promedio=(nota1+nota2+nota3)/3
+let total=((promedio*0.55) + (examenf*0.3) + (trabajof * 0.15));
+console.log(`El promedio de los parciales es: ${promedio} cuyo porcentaje es ${(promedio*0.55)}%`);
+console.log(`La calificacion del examen final es : ${examenf} cuyo porcentaje es ${(examenf*0.3)}`);
+console.log(`La calificacion del trabajo final es : ${trabajof} cuyo porcentaje es ${(trabajof * 0.15)}`);
+console.log(`La nota final es: ${total}`);
+
+
 

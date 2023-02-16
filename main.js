f67e7a796e724dd84f878ed6dd32641cb4b0f251
wili1991 <wikki9106@gmail.com>
2023-02-15 20:22:17 -0500

    titulo tablas

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 let num=prompt("digite el numero del que desea saber la tabla de multiplicar.")
 
 for (let i = 1; i <= num; i++) {
+    console.log(`\n Tabla ${i} \n `);
     for (let j = 0; j < 11; j++) {
         console.log(`${i} X ${j} = ${i*j}`);
     }
